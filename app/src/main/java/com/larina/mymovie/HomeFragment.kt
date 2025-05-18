c0f58da53b291f73d4699bfe14bc83278a22c0fb
AlinaEnston <mashitsinalina@gmail.com>
2025-05-18 15:01:06 +0300

    модуль 27

--- app/src/main/java/com/larina/mymovie/HomeFragment.kt
+++ app/src/main/java/com/larina/mymovie/HomeFragment.kt
@@ -8,13 +8,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import androidx.appcompat.widget.SearchView // Не забудьте импортировать SearchView
+import androidx.appcompat.widget.SearchView
 
 class HomeFragment : Fragment() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var filmsAdapter: FilmListRecyclerAdapter
-    private lateinit var searchView: SearchView // Объявляем переменную для SearchView
+    private lateinit var searchView: SearchView
+
+    private lateinit var filmsDataBase: List<Film>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,9 +26,19 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         recyclerView = view.findViewById(R.id.main_recycler)
-        searchView = view.findViewById(R.id.search_view) // Инициализируем SearchView
+        searchView = view.findViewById(R.id.search_view)
 
-        // Инициализация адаптера с передачей слушателя кликов
+        filmsDataBase = listOf(
+            Film("Aladdin", R.drawable.poster_8, "Aladdin, a kind thief, woos Jasmine, the princess of Agrabah, with the help of Genie. When Jafar, the grand vizier, tries to usurp the king, Jasmine, Aladdin and Genie must stop him from succeeding."),
+            Film("Why Women Kill", R.drawable.poster_7, "An anthology series that follows three women in different decades all living in the same house, as they deal with infidelity and betrayals in their marriages."),
+            Film("Le fabuleux destin d'Amélie Poulain", R.drawable.poster_6, "Despite being caught in her imaginative world, young waitress Amelie decides to help people find happiness. Her quest to spread joy leads her on a journey during which she finds true love."),
+            Film("Luck", R.drawable.poster_5, "The curtain is pulled back on the millennia-old battle between the organizations of good luck and bad luck that secretly affects everyday lives."),
+            Film("Wicked", R.drawable.poster_4, "Elphaba, a young woman ridiculed for her green skin, and Galinda, a popular girl, become friends at Shiz University in the Land of Oz. After an encounter with the Wonderful Wizard of Oz, their friendship reaches a crossroads."),
+            Film("W.I.T.C.H", R.drawable.poster_3, "Five teenage girls learn that they have been chosen to guard the walls between parallel universes. For this purpose, they have been given the powers of the elements."),
+            Film("Paddington", R.drawable.poster_10, "A young Peruvian bear travels to London in search of a home. Finding himself lost and alone at Paddington Station, he meets the kindly Brown family, who offer him a temporary haven."),
+            Film("La La Land", R.drawable.poster_1, "When Sebastian, a pianist, and Mia, an actress, follow their passion and achieve success in their respective fields, they find themselves torn between their love for each other and their careers.")
+        )
+
         filmsAdapter = FilmListRecyclerAdapter(mutableListOf(), object : FilmListRecyclerAdapter.OnItemClickListener {
             override fun click(film: Film) {
                 (requireActivity() as MainActivity).launchDetailsFragment(film)
@@ -40,34 +51,32 @@ class HomeFragment : Fragment() {
             addItemDecoration(TopSpacingItemDecoration(8))
         }
 
-        // Получаем данные фильмов
-        val filmsDataBase = listOf(
-            Film("Aladdin", R.drawable.poster_8, "Aladdin, a kind thief, woos Jasmine, the princess of Agrabah, with the help of Genie. When Jafar, the grand vizier, tries to usurp the king, Jasmine, Aladdin and Genie must stop him from succeeding."),
-            Film("Why Women Kill", R.drawable.poster_7, "An anthology series that follows three women in different decades all living in the same house, as they deal with infidelity and betrayals in their marriages."),
-            Film("Le fabuleux destin d'Amélie Poulain", R.drawable.poster_6, "Despite being caught in her imaginative world, young waitress Amelie decides to help people find happiness. Her quest to spread joy leads her on a journey during which she finds true love."),
-            Film("Luck", R.drawable.poster_5, "The curtain is pulled back on the millennia-old battle between the organizations of good luck and bad luck that secretly affects everyday lives."),
-            Film("Wicked", R.drawable.poster_4, "Elphaba, a young woman ridiculed for her green skin, and Galinda, a popular girl, become friends at Shiz University in the Land of Oz. After an encounter with the Wonderful Wizard of Oz, their friendship reaches a crossroads."),
-            Film("W.I.T.C.H", R.drawable.poster_3, "Five teenage girls learn that they have been chosen to guard the walls between parallel universes. For this purpose, they have been given the powers of the elements."),
-            Film("Paddington", R.drawable.poster_10, "A young Peruvian bear travels to London in search of a home. Finding himself lost and alone at Paddington Station, he meets the kindly Brown family, who offer him a temporary haven."),
-            Film("La La Land", R.drawable.poster_1, "When Sebastian, a pianist, and Mia, an actress, follow their passion and achieve success in their respective fields, they find themselves torn between their love for each other and their careers.")
-        )
-
-        // Передаем данные в адаптер
         filmsAdapter.addItems(filmsDataBase)
 
-        // Обработчик клика для SearchView
         searchView.setOnClickListener {
-            searchView.isIconified = false // Разворачиваем SearchView
+            searchView.isIconified = false
         }
-    }
 
-    class YourBottomSheetFragment : BottomSheetDialogFragment() {
-        override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-        ): View? {
-            return inflater.inflate(R.layout.bottom_sheet, container, false)
-        }
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                // Обработка нажатия кнопки поиска (можно реализовать логику поиска или скрыть клавиатуру)
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                // Фильтрация списка фильмов по введенному тексту
+                val filteredList = if (!newText.isNullOrEmpty()) {
+                    filmsDataBase.filter {
+                        it.title.contains(newText, ignoreCase = true) ||
+                                it.description?.contains(newText, ignoreCase = true) == true // Используем безопасный вызов
+                    }
+                } else {
+                    filmsDataBase
+                }
+                filmsAdapter.updateData(filteredList)
+                return true
+            }
+
+        })
     }
 }
