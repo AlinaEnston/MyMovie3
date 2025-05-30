5e4013a5ac25f7b1d87a186955fca963b8dad1dd
AlinaEnston <mashitsinalina@gmail.com>
2025-05-30 15:45:26 +0300

    Добавлены изменения в разметку и код фрагмента

---
+++ app/src/main/java/com/larina/mymovie/FavoritesFragment.kt
@@ -0,0 +1,56 @@
+package com.larina.mymovie
+
+import FilmListRecyclerAdapter
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class FavoritesFragment : Fragment() {
+
+    private lateinit var favoritesRecycler: RecyclerView
+    private lateinit var filmsAdapter: FilmListRecyclerAdapter
+    private lateinit var favoritesDatabaseHelper: FavoritesDatabaseHelper
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_favorites, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        favoritesDatabaseHelper = FavoritesDatabaseHelper(requireContext())
+        favoritesRecycler = view.findViewById(R.id.favorites_recycler)
+
+        // Получите список избранных фильмов из базы данных
+        val favoriteFilmsList = favoritesDatabaseHelper.getAllFavorites()
+
+        // Инициализируйте адаптер с полученным списком и слушателем кликов
+        filmsAdapter = FilmListRecyclerAdapter(favoriteFilmsList.toMutableList(), object : FilmListRecyclerAdapter.OnItemClickListener {
+            override fun click(film: Film) {
+                (requireActivity() as MainActivity).launchDetailsFragment(film)
+            }
+        })
+
+        // Настройте RecyclerView
+        favoritesRecycler.apply {
+            adapter = filmsAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+            val decorator = TopSpacingItemDecoration(8)
+            addItemDecoration(decorator)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // Обновите данные при возвращении в фрагмент
+        val updatedList = favoritesDatabaseHelper.getAllFavorites()
+        filmsAdapter.updateData(updatedList)
+    }
+}
---
+++ app/src/main/java/com/larina/mymovie/FilmListRecyclerAdapter.kt
@@ -0,0 +1,59 @@
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.larina.mymovie.Film
+import com.larina.mymovie.R
+
+class FilmListRecyclerAdapter(
+    private var filmsList: MutableList<Film>,
+    private val listener: OnItemClickListener
+) : RecyclerView.Adapter<FilmListRecyclerAdapter.FilmViewHolder>() {
+
+    interface OnItemClickListener {
+        fun click(film: Film)
+    }
+
+    class FilmViewHolder(itemView: View, private val listener: OnItemClickListener) : RecyclerView.ViewHolder(itemView) {
+        private val poster: ImageView = itemView.findViewById(R.id.poster)
+        private val title: TextView = itemView.findViewById(R.id.title)
+        private val description: TextView = itemView.findViewById(R.id.description)
+
+        fun bind(film: Film) {
+            poster.setImageResource(film.poster)
+            title.text = film.title
+            description.text = film.description
+
+            itemView.setOnClickListener {
+                listener.click(film)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FilmViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.film_item, parent, false)
+        return FilmViewHolder(view, listener)
+    }
+
+    override fun onBindViewHolder(holder: FilmViewHolder, position: Int) {
+        holder.bind(filmsList[position])
+    }
+
+    override fun getItemCount(): Int = filmsList.size
+
+    // Метод для добавления элементов
+    fun addItems(newFilms: List<Film>) {
+        filmsList.clear()
+        filmsList.addAll(newFilms)
+        notifyDataSetChanged()
+    }
+
+    // Метод для обновления данных
+    fun updateData(newList: List<Film>) {
+        filmsList.clear()
+        filmsList.addAll(newList)
+        notifyDataSetChanged()
+    }
+}
